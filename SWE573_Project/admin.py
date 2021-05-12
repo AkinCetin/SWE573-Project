f3c600843a2eddfd4d86c7e6150fbb9b140ebe7d
Akın Çetin <akincetin@sabanciuniv.edu>
2021-05-12 15:18:56 +0300

    article admin paneliyle ilişkilendirildi

--- SWE573_Project/admin.py
+++ SWE573_Project/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from SWE573_Project.models import TagModel
+from SWE573_Project.models import TagModel, ArticleModel
 
 admin.site.register(TagModel)
+admin.site.register(ArticleModel)

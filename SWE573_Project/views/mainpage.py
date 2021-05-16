3dd94c949d8edfd8909d85af7eab9b73f526f454
Akın Çetin <akincetin@sabanciuniv.edu>
2021-05-16 16:04:16 +0300

    pagination tamamlandı

--- SWE573_Project/views/mainpage.py
+++ SWE573_Project/views/mainpage.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from SWE573_Project.models import ArticleModel
+from django.core.paginator import Paginator
 
 
 def mainpage(request):
     articles = ArticleModel.objects.all()
+    page = request.GET.get('page')
+    paginator = Paginator(articles, 10)
     return render(request, 'pages/mainpage.html', context={
-        'articles': articles
+        'articles': paginator.get_page(page)
     })

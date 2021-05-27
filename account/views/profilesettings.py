5407aa35403a9ee833dffbce3200d051c72cd6a4
Akın Çetin <akincetin@sabanciuniv.edu>
2021-05-27 16:30:35 +0300

    profilde eklenenler vardı

--- account/views/profilesettings.py
+++ account/views/profilesettings.py
@@ -6,7 +6,7 @@ from account.forms import ProfileSettingsForm
 @login_required(login_url='/')
 def profilesettings(request):
     if request.method == 'POST':
-        form = ProfileSettingsForm(request.POST,request.FILES, instance = request.user) #request.FILE çıkardım eklenebilir 
+        form = ProfileSettingsForm(request.POST,request.FILES, instance = request.user) 
         if form.is_valid():
             form.save()
             messages.success(request, 'Successful')

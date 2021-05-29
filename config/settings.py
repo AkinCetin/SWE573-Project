dea5976a7ef74ff070223938d6affa5a2f04d118
Akın Çetin <akincetin@sabanciuniv.edu>
2021-05-29 22:00:54 +0300

    login başarılı

--- config/settings.py
+++ config/settings.py
@@ -137,4 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4' #crispy formslar için bootstrap4 seçildi eklenmeli
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'homepage'

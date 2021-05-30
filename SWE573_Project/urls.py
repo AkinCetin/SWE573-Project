66102b557e0ccdf9d0c66e6c9592b5e870fa6696
Akın Çetin <akincetin@sabanciuniv.edu>
2021-05-30 13:49:07 +0300

    template viewlar tammalandı

--- SWE573_Project/urls.py
+++ SWE573_Project/urls.py
@@ -5,6 +5,7 @@ from SWE573_Project.views import tag
 from SWE573_Project.views import details
 from SWE573_Project.views import report
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 
 
@@ -13,6 +14,9 @@ urlpatterns = [
         template_name = 'pages/login.html'
     ), name= 'login'),
     path('contact', contact),
+    path('aboutus', TemplateView.as_view(
+        template_name='pages/aboutus.html'
+    ), name = 'aboutus'),
     path('homepage', mainpage, name = 'homepage'),
     path('tag/<slug:tagSlug>', tag, name='tag'),
     path('details/<slug:slug>', details, name='details'),

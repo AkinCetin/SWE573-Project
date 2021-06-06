72ebe39ba04312df0604eef0eed3b17a310cf393
Akın Çetin <akincetin@sabanciuniv.edu>
2021-06-06 17:07:47 +0300

    tag işşeminde ilerlendir

--- SWE573_Project/models/TagModel.py
+++ SWE573_Project/models/TagModel.py
@@ -3,6 +3,7 @@ from django.db import models
 class TagModel(models.Model):
     name = models.CharField(max_length=255 ,blank=False)
     user = models.ForeignKey('account.CustomUserModel', on_delete=models.CASCADE ,related_name= 'tags', null=True)
+    
 
 
 

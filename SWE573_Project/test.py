528ff0da82684f4238a7345b8969bb42290980b4
Akin Cetin <akincetin@Akins-MacBook-Pro.local>
2021-06-16 13:51:02 +0300

    test eklemesi yapıldı

--- SWE573_Project/test.py
+++ SWE573_Project/test.py
@@ -4,7 +4,8 @@ from django.db.utils import IntegrityError
 
 class ArticleTestCase(TestCase):
     def setUp(self):
-        ArticleModel.objects.create(title="article title", body="article body", authors='author', pmid='1234', keywords='keyword1', slug='testslug')
+        ArticleModel.objects.create(title='article title', body="article body", authors='author', pmid='1234', keywords='keyword1', slug='testslug')
+        ArticleModel.objects.create(title='article 1', body='body 1', authors='author1', pmid='5678', keywords='keyword1', slug='testslug1')
 
     def test_article_is_created_properly(self):
         article = ArticleModel.objects.get(slug='testslug')
@@ -18,8 +19,25 @@ class ArticleTestCase(TestCase):
         with self.assertRaises(IntegrityError):
             ArticleModel.objects.create(title="article title", body="article body", authors='author', pmid='1234', keywords='keyword1', slug='testslug')
 
+    def test_unique_field_pmid(self):
+        with self.assertRaises(IntegrityError):
+            ArticleModel.objects.create(title='article 2', body='body 2', authors='author2', pmid='5678', keywords='keyword2', slug='testslug')
 
+class TagTestCase(TestCase):
 
+    def setUp(self):
+        TagModel.objects.create(name='testtag', url='testurl.com')
 
+    def test_tag_is_created_properly(self):
+        tag = TagModel.objects.get(name='testtag')
+        self.assertEqual(tag.name, 'testtag')
+        self.assertEqual(tag.url, 'testurl.com')
 
+class ReportTestCase(TestCase):
 
+    def setUp(self):
+        ReportModel.objects.create(message='report test message')
+
+    def test_report_created_properly(self):
+        report = ReportModel.objects.get(message='report test message')
+        self.assertEqual(report.message, 'report test message')

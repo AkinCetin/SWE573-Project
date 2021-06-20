ed860e65afc8e8fcc82b5edc3fb036748a3d2501
Akin Cetin <akincetin@Akins-MacBook-Pro.local>
2021-06-20 15:56:03 +0300

    deployment ayarları

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
